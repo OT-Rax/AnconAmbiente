15bcf731348dfdcfa85ef433779954bfdacbaccc
Rahmi El Mechri <rahmi.elmechri@gmail.com>
2021-08-25 15:03:37 +0200

    Login Gui, sistemato codice e creato primo collegamento

--- src/main/main.py
+++ src/main/main.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-from home.view.VistaHome import VistaHome
+from login.view.VistaLogin import VistaLogin
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    vista_home = VistaHome()
-    vista_home.show()
+    vista_login = VistaLogin()
+    vista_login.show()
     sys.exit(app.exec_())
