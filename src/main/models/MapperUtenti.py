f91169f029abbf084aa7bf4677e8c48d7b2403f1
OT-Rax <rahmi.elmechri@gmail.com>
2022-04-20 22:01:26 +0200

    riprendo

--- src/main/models/MapperUtenti.py
+++ src/main/models/MapperUtenti.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 import hashlib
 
 from models.Utente import Utente
@@ -6,7 +7,10 @@ from models.Utente import Utente
 class MapperUtenti:
     def __init__(self):
         self.db_directory="./db/AAdb"
-        #self.n=
+        self.n=2
+        self.r=16
+        self.p=1
+        self.dklen=64
         
     def check_password(self, username, login_pw):
         con = sqlite3.connect(self.db_directory)
@@ -24,3 +28,25 @@ class MapperUtenti:
                 return True
             else:
                 return False
+
+    def get_utenti(self):
+        con = sqlite3.connect(self.db_directory)
+        cur = con.cursor()
+        utenti = []
+        for row in cur.execute("SELECT * FROM Utenti"):
+            utenti.append(Utente(row[0], row[1], row[2], (row[3], row[4], row[5], row[6], row[7],row[8])))
+        return utenti
+
+
+    def insert_utente(self, utente):
+        con = sqlite3.connect(self.db_directory)
+        cur = con.cursor()
+        permessi = utente.get_permessi()
+        #cur.execute("INSERT INTO Utenti VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (utente.get_id(), utente.get_username(), self.encrypt_password(utente.get_password()))
+        con.commit()
+
+    def encrypt_password(self, password):
+        salt = os.urandom(10)
+        hash = hashlib.scrypt(password.encode(), salt=salt, n=self.n, r=self.r, p=self.p, dklen=self.dklen)
+        encrypted_password = f"{hash}${salt}${self.n}${self.r}${self.p}${self.dklen}"
+        return encrypted_password
--- src/main/models/Utente.py
+++ src/main/models/Utente.py
@@ -1,21 +1,28 @@
 class Utente:
-    def __init__(self, nome, password, permessi):
+    def __init__(self, id, username, password, permessi):
         super(Utente, self).__init__()
-        self.nome = nome
+        self.id = id
+        self.username = username
         self.password = password
         self.permessi = permessi
 
-    def get_nome(self):
-        return self.nome
+    def get_id(self):
+        return self.id
 
+    def get_username(self):
+        return self.username
+    
     def get_password(self):
         return self.password
 
     def get_permessi(self):
         return self.permessi
 
-    def set_nome(self, nome):
-        self.nome = nome
+    def set_id(self, id):
+        self.id = id
+
+    def set_username(self, username):
+        self.username = username
 
     def set_password(self, password):
         self.password = password
