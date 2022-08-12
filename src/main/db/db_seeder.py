98ccc1265acb474981c1dc80efc3e1e8e1bbe1c2
Riccardo Iobbi <s1093212@studenti.univpm.it>
2022-08-12 16:31:07 +0200

    Faker per mezzi

--- src/main/db/db_seeder.py
+++ src/main/db/db_seeder.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 from faker import Faker
 
 if __name__ == '__main__':
@@ -12,13 +13,22 @@ if __name__ == '__main__':
     #                           ('Gordon', 'Sbordon', '2022-11-11', '0123456789123456', NULL, 0)
     # ''')
     for i in range(10):
-        row = [fake.first_name(), fake.last_name(), fake.date_of_birth(),
-               fake.ssn(), fake.future_date(), fake.random_int(min=0, max=1)]
+        operatore = [fake.first_name(), fake.last_name(), fake.date_of_birth(),
+               fake.ssn(), fake.future_date(), fake.random_int(min=0, max=2)]
         cur.execute(' \
                 INSERT INTO Operatori (nome, cognome, data_nascita, cf, data_fine_contratto, stato) \
                 VALUES (?, ?, ?, ?, ?, ?); \
-                ', row)
+                ', operatore)
 
+    for i in range(10):
+        patenti = ["B", "B1", "C"]
+        tipo = ["Auto", "Autoarticolato", "Camion", "Camioncino"]
+        mezzo = [fake.license_plate(), random.choice(patenti), random.choice(tipo),
+                 fake.past_date(), fake.random_int(min=0, max=2)]
+        cur.execute(' \
+                    INSERT INTO Mezzi (targa, livello_richiesto, tipo, iscrizione_albo, stato) \
+                    VALUES (?, ?, ?, ?, ?); \
+                    ', mezzo)
     con.commit()
     con.close()
 
