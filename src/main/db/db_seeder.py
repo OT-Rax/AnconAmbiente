833998a938a540486021f79cc2b8d96fa5cd4d66
Riccardo Iobbi <s1093212@studenti.univpm.it>
2022-08-17 14:19:47 +0200

    Seeding abilitazioni

--- src/main/db/db_seeder.py
+++ src/main/db/db_seeder.py
@@ -172,6 +172,26 @@ if __name__ == '__main__':
 
     print("-------------------------")
 
+    # Popolazione Abilitazioni
+    print("Popolazione Abilitazioni")
+
+    for i in range(10):
+
+        try:
+            print("Inserimento abilitazione numero", i)
+            patente_random = random.choice(patenti)
+            operatore_random = id_operatori[fake.random_int(min=0, max=len(id_operatori) - 1)][0]
+            cur.execute(' \
+                                                              INSERT INTO Abilitazioni (livello_patente, id_operatore) \
+                                                              VALUES (?, ?); \
+                                                              ', (patente_random, operatore_random))
+        except sqlite3.IntegrityError:
+            print("Abilitazione numero ", i, " gia presente")
+        except Exception as e:
+            print(e)
+
+    print("-------------------------")
+
     con.commit()
     con.close()
 
