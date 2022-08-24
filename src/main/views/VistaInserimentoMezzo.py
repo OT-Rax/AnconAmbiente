c5493f804cb7cb045ed7ebcecb14d8b06dd6051d
Lorenzo Cardo <cardonilorenzo31@gmail.com>
2022-08-24 17:46:18 +0200

    Aggiornamento gui mezzi

--- src/main/views/VistaInserimentoMezzo.py
+++ src/main/views/VistaInserimentoMezzo.py
@@ -19,10 +19,42 @@ class VistaInserimentoMezzo(QtWidgets.QMainWindow):
         #Inserisci controllo validita caratteri, lunghezza e coerenza
         targa=self.targa_field.text()
         modello=self.modello_field.text()
-        funzione=self.funzione_field.text()
+        allestimento=self.allestimento_field.text()
         data_iscrizione=self.iscrizione_datepicker.date()
         patente=self.patente_combo.currentText()
-        self.controller.insert_mezzo(targa, modello, funzione, data_iscrizione.toString("yyyy-MM-dd"), patente , 0)
-        #finestra pop up a buon fine
-        self.close
-        self.parent().update()
+        targa_validity = self.check_targa()
+        modello_validity = self.check_modello()
+        allestimento_validity = self.check_allestimento()
+        if targa_validity and modello_validity and allestimento_validity:
+            #try:
+            self.controller.insert_mezzo(targa, modello, allestimento, data_iscrizione.toString("yyyy-MM-dd"), patente, 0)
+            #finestra pop up a buon fine
+            self.close()
+            self.parent().update()
+            #except:
+                #finestra pop up qualcosa e andato storto;
+                #print("Qualcosa non va nell'inserimento")
+
+    def check_targa(self):
+        if len(self.targa_field.text()) == 0:
+            self.targa_error.setText("Inserire targa del mezzo")
+            return False
+        else:
+            self.targa_error.setText("")
+            return True
+
+    def check_modello(self):
+        if len(self.modello_field.text()) == 0:
+            self.modello_error.setText("Inserire modello del mezzo")
+            return False
+        else:
+            self.modello_error.setText("")
+            return True
+
+    def check_allestimento(self):
+        if len(self.allestimento_field.text()) == 0:
+            self.allestimento_error.setText("Inserire l'allestimento del mezzo")
+            return False
+        else:
+            self.allestimento_error.setText("")
+            return True
