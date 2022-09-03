bdb297efe019f8109b7bbcacdd55f8a3157fb9c8
Lorenzo Cardo <cardonilorenzo31@gmail.com>
2022-09-03 23:38:50 +0200

    servizio

--- src/main/views/VistaServizio.py
+++ src/main/views/VistaServizio.py
@@ -3,17 +3,19 @@ import sys
 import xz_rc
 
 from views.VistaModificaServizio import VistaModificaServizio
+from controllers.ControlloreClienti import ControlloreClienti
 
 
 class VistaServizio(QtWidgets.QMainWindow):
     def __init__(self, parent, servizio):
         super(VistaServizio, self).__init__(parent)  # Call the inherited classes __init__ method
         uic.loadUi('gui/visualizza_servizio.ui', self)  # Load the .ui file
+        self.controller_clienti = ControlloreClienti()
         self.servizio = servizio
         self.parent = parent
         self.id_label.setText(str(servizio.get_id()))
         self.tipo_label.setText(servizio.get_tipo())
-        self.cliente_label.setText(str(servizio.get_id_cliente()))
+        self.cliente_label.setText(self.controller_clienti.get_cliente(servizio.get_id_cliente()).get_nome()+" "+self.controller_clienti.get_cliente(servizio.get_id_cliente()).get_cognome())
         self.luogo_label.setText(str(servizio.get_luogo())) 
         self.periodicita_label.setText("Non periodico" if servizio.get_periodicita() is None else str(servizio.get_ripetizione())+" volte "+servizio.get_periodicita())
         self.indietro_button.clicked.connect(self.close)
