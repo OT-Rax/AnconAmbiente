9fde50864b02b6047c1d8c8f869444b1b1fcce29
OT-Rax <rahmi.elmechri@gmail.com>
2022-09-05 01:26:51 +0200

    inizio path relativi

---
+++ src/main/PyUnit/TestAAERP.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestAAERP (unittest.TestCase):
+
+    def test_login(self):
+        self.assertTrue(True)
+
+    def test_inserimento_operatore(self):
+
+    def test_ricerca_operatore(self):
+
+    def test_elimina_operatore(self):
+    
+    def test_update_operatore(self):
+
+if __name__ == '__main__':
+    unittest.main()
--- src/main/main.py
+++ src/main/main.py
@@ -1,11 +1,14 @@
 import sys
 import sqlite3
+import os
 from PyQt5.QtWidgets import QApplication
 
 from views.VistaLogin import VistaLogin
 
 if __name__ == '__main__':
-    con=sqlite3.connect('db/AAdb')
+    dirname = os.path.dirname(__file__)
+    db_file = os.path.join(dirname, 'db/AAdb')
+    con=sqlite3.connect(db_file)
     #cur = con.execute("query")
     con.commit()
     app = QApplication(sys.argv)
